0815061af54bb55cf5168b3dbaf20d24fe60530f
Sohee <sh11govlday@gmail.com>
2024-11-11 05:56:23 +0900

    test : 구매할 상품 및 수량 입력 예외 테스트 추가

---
+++ __tests__/InputView.test.js
@@ -0,0 +1,7 @@
+import InputView from '../src/io/InputView.js';
+
+test('구매할 상품 및 수량 입력 예외 테스트', () => {
+  expect(() => {
+    InputView.inputValidate('[사이다 2],[컵라면-1]');
+  }).toThrow('[ERROR]');
+});
