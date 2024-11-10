5d2b6ffe7918c5b225f99473d7c09a49cc0f42a6
Sohee <sh11govlday@gmail.com>
2024-11-11 06:09:00 +0900

    feat : 구매 상품에 대한 예외 처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
 import InputView from './io/InputView.js';
 import OutputView from './io/OutputView.js';
 import StockManager from './stock-manager/StockManager.js';
@@ -8,7 +9,21 @@ class App {
     await stockManager.loadProducts();
 
     OutputView.printProducts(stockManager.products);
-    let items = await InputView.readItems();
+    let items = await this.requestItems(stockManager);
+    console.log(items);
+  }
+
+  async requestItems(stockManager) {
+    while (true) {
+      try {
+        let items = await InputView.readItems();
+        stockManager.validatePurchase(items);
+        items = stockManager.addPriceToItems(items);
+        return items;
+      } catch (error) {
+        MissionUtils.Console.print(error.message);
+      }
+    }
   }
 }
 
--- src/stock-manager/StockManager.js
+++ src/stock-manager/StockManager.js
@@ -10,6 +10,52 @@ class StockManager {
   get products() {
     return this.#products;
   }
+
+  validatePurchase(items) {
+    for (const item of items) {
+      const products = this.#products.filter(
+        (product) => product.name === item.name
+      );
+      this.checkItemExist(products);
+      this.checkItemQuantity(products, item);
+    }
+  }
+
+  checkItemExist(products) {
+    if (products.length === 0)
+      throw new Error('[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.');
+  }
+
+  checkItemQuantity(products, item) {
+    const totalQuantity = this.calculateTotalQuantity(products);
+    if (totalQuantity < item.quantity)
+      throw new Error(
+        '[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.'
+      );
+  }
+
+  calculateTotalQuantity(products) {
+    let normalQuantity = 0;
+    let promotionQuantity = 0;
+
+    products.forEach((product) => {
+      if (product.promotion) promotionQuantity += product.quantity;
+      else normalQuantity += product.quantity;
+    });
+
+    return normalQuantity + promotionQuantity;
+  }
+
+  addPriceToItems(items) {
+    let updatedItems = [];
+    for (const item of items) {
+      let product = this.#products.find(
+        (product) => product.name === item.name
+      );
+      updatedItems.push({ ...item, price: product.price });
+    }
+    return updatedItems;
+  }
 }
 
 export default StockManager;
