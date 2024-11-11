c42838fb2eb7a3bfe0c1575cf3b3fe920e3ffa33
Sohee <sh11govlday@gmail.com>
2024-11-11 17:10:17 +0900

    test : Y/N 답변 예외 테스트 추가

--- __tests__/InputView.test.js
+++ __tests__/InputView.test.js
@@ -5,3 +5,9 @@ test('구매할 상품 및 수량 입력 예외 테스트', () => {
     InputView.inputValidate('[사이다 2],[컵라면-1]');
   }).toThrow('[ERROR]');
 });
+
+test('Y/N 답변 입력 예외 테스트', () => {
+  expect(() => {
+    InputView.isAnswerValidate('d');
+  }).toThrow('[ERROR]');
+});
